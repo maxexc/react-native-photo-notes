19d50f491a3c5225a741a99d131e4f3fbffc1074
Maksym Solomko <max.exc7@gmail.com>
2023-07-05 13:52:52 +0200

    Update store.js

--- redux/store.js
+++ redux/store.js
@@ -1,10 +1,13 @@
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import { authSlice } from "./auth/authSlice";
 
-const rootReduce = combineReducers({
-    [authSlice.name]: authSlice.reducer,
-});
+// const rootReduce = combineReducers({
+//     [authSlice.name]: authSlice.reducer,
+// });
 
 export const store = configureStore({
-    reducer: rootReduce,
+    // reducer: rootReduce,
+    reducer: {
+        [authSlice.name]: authSlice.reducer,
+    }
 });
